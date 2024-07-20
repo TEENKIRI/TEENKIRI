a8cf65eda65242ae06392e66e291a92bf1353c71
milcho0604 <milcho0604@gmail.com>
2024-07-20 16:56:40 +0900

    init commit

---
+++ board/board_demo/src/main/java/com/teeni/board_demo/BoardDemoApplication.java
@@ -0,0 +1,13 @@
+package com.teeni.board_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardDemoApplication.class, args);
+    }
+
+}
