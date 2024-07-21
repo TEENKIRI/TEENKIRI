33c0f57cd9b260390e278fc0856357e8a27c082a
milcho0604 <milcho0604@gmail.com>
2024-07-21 19:24:19 +0900

    comment QnA

--- board_demo/src/main/java/com/beyond/board_demo/comment/service/CommentService.java
+++ board_demo/src/main/java/com/beyond/board_demo/comment/service/CommentService.java
@@ -6,6 +6,8 @@ import com.beyond.board_demo.comment.dto.CommentSaveReqDto;
 import com.beyond.board_demo.comment.repository.CommentRepository;
 import com.beyond.board_demo.post.domain.Post;
 import com.beyond.board_demo.post.repository.PostRepository;
+import com.beyond.board_demo.qna.domain.QnA;
+import com.beyond.board_demo.qna.repository.QnARepository;
 import com.beyond.board_demo.user.domain.User;
 import com.beyond.board_demo.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,27 +22,37 @@ import java.util.stream.Collectors;
 public class CommentService {
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
+    private final QnARepository qnaRepository;
     private final UserRepository userRepository;
 
     @Autowired
-    public CommentService(CommentRepository commentRepository, PostRepository postRepository, UserRepository userRepository) {
+    public CommentService(CommentRepository commentRepository, PostRepository postRepository, QnARepository qnaRepository, UserRepository userRepository) {
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
+        this.qnaRepository = qnaRepository;
         this.userRepository = userRepository;
     }
 
     @Transactional
     public Comment saveComment(CommentSaveReqDto dto) {
-        Post post = postRepository.findById(dto.getPostId())
-                .orElseThrow(() -> new EntityNotFoundException("Post not found"));
-
         User user = userRepository.findByEmail(dto.getUserEmail())
                 .orElseThrow(() -> new EntityNotFoundException("User not found"));
 
         Comment comment = new Comment();
         comment.setContent(dto.getContent());
-        comment.setPost(post);
         comment.setUser(user);
+
+        if (dto.getPostId() != null) {
+            Post post = postRepository.findById(dto.getPostId())
+                    .orElseThrow(() -> new EntityNotFoundException("Post not found"));
+            comment.setPost(post);
+        } else if (dto.getQnaId() != null) {
+            QnA qna = qnaRepository.findById(dto.getQnaId())
+                    .orElseThrow(() -> new EntityNotFoundException("QnA not found"));
+            comment.setQna(qna);
+        } else {
+            throw new IllegalArgumentException("Either postId or qnaId must be provided");
+        }
 
         return commentRepository.save(comment);
     }
@@ -56,4 +68,17 @@ public class CommentService {
                         .build())
                 .collect(Collectors.toList());
     }
+
+    public List<CommentDetailDto> getCommentsByQnaId(Long qnaId) {
+        List<Comment> comments = commentRepository.findByQnaId(qnaId);
+        return comments.stream()
+                .map(comment -> CommentDetailDto.builder()
+                        .id(comment.getId())
+                        .content(comment.getContent())
+                        .userEmail(comment.getUser().getEmail())
+                        .createdTime(comment.getCreatedTime())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
+
