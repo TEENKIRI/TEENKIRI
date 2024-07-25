202f5d301a6bf56f1c1d71418f5c856a7996390e
JUNG-EUN1997 <rro0628@gmail.com>
2024-07-26 08:53:36 +0900

    spring setting

---
+++ teenkiri/src/test/java/com/beyond/teenkiri/teenkiri/TeenkiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.teenkiri.teenkiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeenkiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
