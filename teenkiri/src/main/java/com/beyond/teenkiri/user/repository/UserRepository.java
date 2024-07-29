3baab8fe8749d1ecc727c495b703c5b37f34c926
JUNG-EUN1997 <rro0628@gmail.com>
2024-07-29 14:56:47 +0900

    feat : 창현님 user 소스 추가

--- teenkiri/src/main/java/com/beyond/teenkiri/user/repository/UserRepository.java
+++ teenkiri/src/main/java/com/beyond/teenkiri/user/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.beyond.teenkiri.user.repository;
 
 import com.beyond.teenkiri.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByEmail(String email);
-
+    Optional<User> findByEmail(String email);
 }
