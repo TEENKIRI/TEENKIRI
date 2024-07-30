12477d3bbab7a9314fb76567fa5a750c52b26427
JUNG-EUN1997 <rro0628@gmail.com>
2024-07-30 23:17:02 +0900

    merge : board_demo ver 1

--- teenkiri/src/main/java/com/beyond/teenkiri/user/dto/UserSaveReqDto.java
+++ teenkiri/src/main/java/com/beyond/teenkiri/user/dto/UserSaveReqDto.java
@@ -1,6 +1,6 @@
 package com.beyond.teenkiri.user.dto;
 
-import com.beyond.teenkiri.common.DelYN;
+import com.beyond.teenkiri.common.domain.DelYN;
 import com.beyond.teenkiri.user.domain.Role;
 import com.beyond.teenkiri.user.domain.User;
 import lombok.AllArgsConstructor;
@@ -8,9 +8,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import java.util.ArrayList;
 
 @Data
@@ -22,13 +19,13 @@ public class UserSaveReqDto {
     private String nickname;
     private String email;
     private String password;
-
     // 사용자가 String 요청해도 Role 클래스 자동 형변환
     @Builder.Default
     private Role role = Role.STUDENT;
     private String phone;
     private String address;
-    private DelYN delYN;
+    @Builder.Default
+    private DelYN delYN = DelYN.N;
 
     // DTO -> Entity 변환
     public User toEntity() {
@@ -38,7 +35,7 @@ public class UserSaveReqDto {
                 .nickname(this.nickname)
                 .email(this.email)
                 .role(this.role)
-//                .posts(new ArrayList<>())//
+                .posts(new ArrayList<>())//
                 .address(this.address)
                 .delYN(this.delYN)
                 .phone(this.phone)
--- teenkiri/src/main/java/com/beyond/teenkiri/user/service/UserService.java
+++ teenkiri/src/main/java/com/beyond/teenkiri/user/service/UserService.java
@@ -1,17 +1,21 @@
 package com.beyond.teenkiri.user.service;
 
 import com.beyond.teenkiri.user.domain.User;
+import com.beyond.teenkiri.user.dto.UserDetailDto;
+import com.beyond.teenkiri.user.dto.UserListResDto;
 import com.beyond.teenkiri.user.dto.UserSaveReqDto;
+import com.beyond.teenkiri.user.dto.UserUpdateDto;
 import com.beyond.teenkiri.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
 
@@ -27,26 +31,24 @@ public class UserService {
             throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
         }
         User user = dto.toEntity();
-
         // cascade persist 테스트, remove 테스트는 회원삭제도 대체
-
         User savedUser = userRepository.save(user);
         return savedUser;
     }
 
-//    // 사용자 리스트
-//    public Page<UserListResDto> userList(Pageable pageable) {
-//        Page<User> users = userRepository.findAll(pageable);
-//        Page<UserListResDto> userListResDtos = users.map(a -> a.listFromEntity());
-//        return userListResDtos;
-//    }
-//
-//    public UserDetailDto userDetail(Long id) {
-//        User user = userRepository.findById(id)
-//                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
-//        System.out.println(user.detailFromEntity());
-//        return user.detailFromEntity();
-//    }
+    // 사용자 리스트
+    public Page<UserListResDto> userList(Pageable pageable) {
+        Page<User> users = userRepository.findAll(pageable);
+        Page<UserListResDto> userListResDtos = users.map(a -> a.listFromEntity());
+        return userListResDtos;
+    }
+
+    public UserDetailDto userDetail(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+        System.out.println(user.detailFromEntity());
+        return user.detailFromEntity();
+    }
 
     public User findByEmail(String email) {
         User user = userRepository.findByEmail(email)
@@ -54,15 +56,15 @@ public class UserService {
         return user;
     }
 
-//    @Transactional
-//    public void userUpdate(Long id, UserUpdateDto dto) {
-//        User user = userRepository.findById(id)
-//                .orElseThrow(() -> new EntityNotFoundException("User is not found"));
-//        user.toUpdate(dto);
-//    }
+    @Transactional
+    public void userUpdate(Long id, UserUpdateDto dto) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User is not found"));
+        user.toUpdate(dto);
+    }
 
-//    @Transactional
-//    public void userDelete(Long id) {
-//        userRepository.deleteById(id);
-//    }
+    @Transactional
+    public void userDelete(Long id) {
+        userRepository.deleteById(id);
+    }
 }
