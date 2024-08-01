7849fcd029e1c4fe4b80ca49e14ca60b4acf6411
JUNG-EUN1997 <rro0628@gmail.com>
2024-08-01 18:18:19 +0900

    feat : 과목 ssr 버전 소스코드 추가

---
+++ teenkiri/src/main/java/com/beyond/teenkiri/course/controller/CourseSsrController.java
@@ -0,0 +1,52 @@
+package com.beyond.teenkiri.course.controller;
+
+import com.beyond.teenkiri.common.dto.CommonResDto;
+import com.beyond.teenkiri.course.domain.Course;
+import com.beyond.teenkiri.course.dto.CourseListResDto;
+import com.beyond.teenkiri.course.dto.CourseSaveReqDto;
+import com.beyond.teenkiri.course.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseSsrController {
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseSsrController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+
+    //    과목 리스트
+    @GetMapping("/course/list")
+    public ResponseEntity<?> courseListView(@PageableDefault(page = 0, size=10, sort = "id",
+            direction = Sort.Direction.DESC ) Pageable pageable){
+        Page<CourseListResDto> courseListResDtos = courseService.courseList(pageable);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "return subject list",courseListResDtos);
+        return new ResponseEntity<>(commonResDto,HttpStatus.OK);
+    }
+
+
+    // 과목 생성
+    @PostMapping("/course/create")
+    public ResponseEntity<?> courseCreate(@RequestBody CourseSaveReqDto dto){
+        Course course = courseService.courseCreate(dto);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.CREATED,"강좌 생성이 완료되었습니다.",course.getId());
+        return new ResponseEntity<>(commonResDto,HttpStatus.CREATED);
+    }
+
+
+//    해당 과목에 연결된 강좌 보기
+
+
+}
