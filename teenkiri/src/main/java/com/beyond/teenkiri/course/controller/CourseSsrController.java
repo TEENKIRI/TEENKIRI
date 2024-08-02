452f674b12ca8baeceaac94416422ec473c10d87
JUNG-EUN1997 <rro0628@gmail.com>
2024-08-02 12:43:10 +0900

    feat : 과목 생성 및 리스트 상세 기능 추가

--- teenkiri/src/main/java/com/beyond/teenkiri/course/controller/CourseSsrController.java
+++ teenkiri/src/main/java/com/beyond/teenkiri/course/controller/CourseSsrController.java
@@ -2,6 +2,7 @@ package com.beyond.teenkiri.course.controller;
 
 import com.beyond.teenkiri.common.dto.CommonResDto;
 import com.beyond.teenkiri.course.domain.Course;
+import com.beyond.teenkiri.course.dto.CourseDetResDto;
 import com.beyond.teenkiri.course.dto.CourseListResDto;
 import com.beyond.teenkiri.course.dto.CourseSaveReqDto;
 import com.beyond.teenkiri.course.service.CourseService;
@@ -12,12 +13,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.persistence.EntityNotFoundException;
+
+@Controller
+@RequestMapping("/ssr")
 public class CourseSsrController {
     private final CourseService courseService;
 
@@ -29,20 +32,38 @@ public class CourseSsrController {
 
     //    과목 리스트
     @GetMapping("/course/list")
-    public ResponseEntity<?> courseListView(@PageableDefault(page = 0, size=10, sort = "id",
-            direction = Sort.Direction.DESC ) Pageable pageable){
+    public String courseListView(@PageableDefault(page = 0, size=10, sort = "id",
+            direction = Sort.Direction.DESC ) Pageable pageable, Model model){
         Page<CourseListResDto> courseListResDtos = courseService.courseList(pageable);
-        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "return subject list",courseListResDtos);
-        return new ResponseEntity<>(commonResDto,HttpStatus.OK);
+        model.addAttribute("courseList", courseListResDtos);
+        return "course/list";
     }
 
+    // 과목 상세페이지 화면
+    @GetMapping("/course/detail/{id}")
+    public String courseDetail(@PathVariable Long id, Model model){
+        CourseDetResDto courseDetResDto = courseService.courseDetail(id);
+        model.addAttribute("course", courseDetResDto);
+        return "course/detail";
+    }
+
+    // 과목 생성 화면
+    @GetMapping("/course/create")
+    public String courseCreateView(){
+        return "course/create";
+    }
 
     // 과목 생성
     @PostMapping("/course/create")
-    public ResponseEntity<?> courseCreate(@RequestBody CourseSaveReqDto dto){
-        Course course = courseService.courseCreate(dto);
-        CommonResDto commonResDto = new CommonResDto(HttpStatus.CREATED,"강좌 생성이 완료되었습니다.",course.getId());
-        return new ResponseEntity<>(commonResDto,HttpStatus.CREATED);
+    public String courseCreate(CourseSaveReqDto dto, Model model){
+        
+        try {
+            Course course = courseService.courseCreate(dto);
+            return "redirect:/ssr/course/list";
+        } catch (SecurityException | EntityNotFoundException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "course/create";
+        }
     }
 
 
--- teenkiri/src/main/java/com/beyond/teenkiri/course/service/CourseService.java
+++ teenkiri/src/main/java/com/beyond/teenkiri/course/service/CourseService.java
@@ -1,6 +1,7 @@
 package com.beyond.teenkiri.course.service;
 
 import com.beyond.teenkiri.course.domain.Course;
+import com.beyond.teenkiri.course.dto.CourseDetResDto;
 import com.beyond.teenkiri.course.dto.CourseListResDto;
 import com.beyond.teenkiri.course.dto.CourseSaveReqDto;
 import com.beyond.teenkiri.course.repository.CourseRepository;
@@ -10,6 +11,7 @@ import com.beyond.teenkiri.subject.dto.SubjectSaveReqDto;
 import com.beyond.teenkiri.user.domain.Role;
 import com.beyond.teenkiri.user.domain.User;
 import com.beyond.teenkiri.user.repository.UserRepository;
+import com.beyond.teenkiri.user.service.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -21,31 +23,39 @@ import javax.persistence.EntityNotFoundException;
 @Transactional
 public class CourseService {
     private final CourseRepository courseRepository;
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public CourseService(CourseRepository courseRepository, UserRepository userRepository) {
+    public CourseService(CourseRepository courseRepository, UserService userService) {
         this.courseRepository = courseRepository;
-        this.userRepository = userRepository;
+        this.userService = userService;
     }
 
 
 //    과목 리스트
     public Page<CourseListResDto> courseList(Pageable pageable){
         Page<Course> courses = courseRepository.findAll(pageable);
-        Page<CourseListResDto> courseListResDtos = courses.map(a->a.fromEntity());
+        Page<CourseListResDto> courseListResDtos = courses.map(a->a.fromListEntity());
         return courseListResDtos;
+    }
+
+//    과목 상세
+    public CourseDetResDto courseDetail(Long id){
+        Course course = courseRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("없는 과목입니다."));
+        CourseDetResDto courseDetResDto = course.fromDetEntity();
+        return courseDetResDto;
     }
 
 //    과목 생성
     public Course courseCreate(CourseSaveReqDto dto){
 //        🚨추후 멤버..
-        User user = userRepository.findById(dto.getUserId()).orElseThrow(()-> new EntityNotFoundException("없는 유저입니다."));
+        User user = userService.findByEmail(dto.getUserEmail());
 
         if(!user.getRole().equals(Role.ADMIN)){ // 관리자 레벨만 강좌를 생성할 수 있도록 권한설정
             throw new IllegalArgumentException("권한이 부족합니다.");
         }
 
-        Course course = dto.toEntity();
+        Course course = dto.toEntity(user);
         courseRepository.save(course);
 
         return course; // save된 subject return;
