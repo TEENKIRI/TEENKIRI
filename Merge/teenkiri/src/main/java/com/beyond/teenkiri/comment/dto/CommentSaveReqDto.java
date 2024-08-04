0d2e58cfb503fd111d6728202883b1acafaae7fc
milcho0604 <milcho0604@gmail.com>
2024-08-04 14:35:40 +0900

    refactor: comment service

--- Merge/teenkiri/src/main/java/com/beyond/teenkiri/comment/dto/CommentSaveReqDto.java
+++ Merge/teenkiri/src/main/java/com/beyond/teenkiri/comment/dto/CommentSaveReqDto.java
@@ -1,6 +1,9 @@
 package com.beyond.teenkiri.comment.dto;
 
+import com.beyond.teenkiri.comment.domain.Comment;
 import com.beyond.teenkiri.common.domain.DelYN;
+import com.beyond.teenkiri.post.domain.Post;
+import com.beyond.teenkiri.qna.domain.QnA;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,4 +20,23 @@ public class CommentSaveReqDto {
     private String content;
     @Builder.Default
     private DelYN delYn = DelYN.N;
+
+
+    public Comment PostToEntity(com.beyond.teenkiri.user_board.domain.user user, Post post){
+        return Comment.builder()
+                .content(this.content)
+                .delYN(this.delYn)
+                .user(user)
+                .post(post)
+                .build();
+    }
+
+    public Comment QnAToEntity(com.beyond.teenkiri.user_board.domain.user user, QnA qnA){
+        return Comment.builder()
+                .content(this.content)
+                .delYN(this.delYn)
+                .user(user)
+                .qna(qnA)
+                .build();
+    }
 }
