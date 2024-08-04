c3a1052c65b803eb0fee644904cad8f2ce81e838
milcho0604 <milcho0604@gmail.com>
2024-08-04 15:44:21 +0900

    refactory 리다이렉트 수정

--- Merge/teenkiri/src/main/java/com/beyond/teenkiri/user_board/controller/UserController.java
+++ Merge/teenkiri/src/main/java/com/beyond/teenkiri/user_board/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller("userBoardController")
-@RequestMapping("user/board")
+@RequestMapping("board/user/")
 public class UserController {
     private final UserService userService;
 
