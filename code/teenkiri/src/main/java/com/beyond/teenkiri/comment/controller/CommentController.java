d97a2e205cb5d826fa1ecee16e2e8202b71c0121
milcho0604 <milcho0604@gmail.com>
2024-08-06 13:53:10 +0900

    refactor: commnetController postmanVer

--- code/teenkiri/src/main/java/com/beyond/teenkiri/comment/controller/CommentController.java
+++ code/teenkiri/src/main/java/com/beyond/teenkiri/comment/controller/CommentController.java
@@ -2,14 +2,16 @@ package com.beyond.teenkiri.comment.controller;
 
 import com.beyond.teenkiri.comment.dto.CommentSaveReqDto;
 import com.beyond.teenkiri.comment.service.CommentService;
+import com.beyond.teenkiri.common.dto.CommonErrorDto;
+import com.beyond.teenkiri.common.dto.CommonResDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.persistence.EntityNotFoundException;
+
+@RestController
 @RequestMapping("comment")
 public class CommentController {
 
@@ -20,19 +22,25 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-//    공지사항은 추가하지 않음
-    @PostMapping("create")
-    public String createComment(@ModelAttribute CommentSaveReqDto dto, RedirectAttributes redirectAttributes) {
-        commentService.saveComment(dto);
-        if (dto.getPostId() != null) {
-            redirectAttributes.addAttribute("id", dto.getPostId());
-            return "redirect:/post/detail/{id}";
-        } else if (dto.getQnaId() != null) {
-            redirectAttributes.addAttribute("id", dto.getQnaId());
-            return "redirect:/qna/detail/{id}";
-        } else {
-            throw new IllegalArgumentException("Either postId or qnaId must be provided for redirection");
+    @PostMapping("/create")
+    public ResponseEntity<?> createComment(@RequestBody CommentSaveReqDto dto) {
+        try {
+            commentService.saveComment(dto);
+            CommonResDto commonResDto;
+
+            if (dto.getPostId() != null) {
+                commonResDto = new CommonResDto(HttpStatus.CREATED, "댓글이 성공적으로 등록되었습니다.", dto.getPostId());
+                return new ResponseEntity<>(commonResDto, HttpStatus.CREATED);
+            } else if (dto.getQnaId() != null) {
+                commonResDto = new CommonResDto(HttpStatus.CREATED, "댓글이 성공적으로 등록되었습니다.", dto.getQnaId());
+                return new ResponseEntity<>(commonResDto, HttpStatus.CREATED);
+            } else {
+                throw new IllegalArgumentException("Either postId or qnaId must be provided.");
+            }
+        } catch (IllegalArgumentException | EntityNotFoundException e) {
+            e.printStackTrace();
+            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST, e.getMessage());
+            return new ResponseEntity<>(commonErrorDto, HttpStatus.BAD_REQUEST);
         }
     }
-
 }
