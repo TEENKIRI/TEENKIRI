f62305404d23c59fe8d22d1e159b0bad03b5bd28
milcho0604 <milcho0604@gmail.com>
2024-08-06 10:12:03 +0900

    refactor: notice_postVer, 작성자 본인만 글을 수정할 수 있도록 권한 수정

--- code/teenkiri/src/main/java/com/beyond/teenkiri/notice/controller/NoticeController.java
+++ code/teenkiri/src/main/java/com/beyond/teenkiri/notice/controller/NoticeController.java
@@ -1,20 +1,25 @@
 package com.beyond.teenkiri.notice.controller;
 
+import com.beyond.teenkiri.common.dto.CommonErrorDto;
+import com.beyond.teenkiri.common.dto.CommonResDto;
+import com.beyond.teenkiri.notice.domain.Notice;
 import com.beyond.teenkiri.notice.dto.NoticeDetailDto;
+import com.beyond.teenkiri.notice.dto.NoticeListResDto;
 import com.beyond.teenkiri.notice.dto.NoticeSaveReqDto;
 import com.beyond.teenkiri.notice.dto.NoticeUpdateDto;
 import com.beyond.teenkiri.notice.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 
-@Controller
+@RestController
 @RequestMapping("notice")
 public class NoticeController {
 
@@ -25,44 +30,62 @@ public class NoticeController {
         this.noticeService = noticeService;
     }
 
-    @GetMapping("create")
-    public String createNoticeForm() {
-        return "board/notice/create";
-    }
-
-    @PostMapping("create")
-    public String createNotice(@ModelAttribute NoticeSaveReqDto dto, Model model) {
+    @PostMapping("/create")
+    public ResponseEntity<?> createNotice(@ModelAttribute NoticeSaveReqDto dto) {
         try {
-            noticeService.createNotice(dto);
-            return "redirect:/notice/list";
+            Notice notice = noticeService.createNotice(dto);
+            CommonResDto commonResDto = new CommonResDto(HttpStatus.CREATED, "공지사항이 성공적으로 등록되었습니다.", notice.getId());
+            return new ResponseEntity<>(commonResDto, HttpStatus.CREATED);
         } catch (SecurityException | EntityNotFoundException e) {
-            model.addAttribute("errorMessage", e.getMessage());
-            return "board/notice/create";
+            e.printStackTrace();
+            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST, e.getMessage());
+            return new ResponseEntity<>(commonErrorDto, HttpStatus.BAD_REQUEST);
         }
     }
 
-    @GetMapping("list")
-    public String getAllNotices(Model model, @PageableDefault(size = 10, sort = "createdTime", direction = Sort.Direction.DESC) Pageable pageable) {
-        model.addAttribute("noticeList", noticeService.noticeList(pageable));
-        return "board/notice/list";
+    @GetMapping("/list")
+    public ResponseEntity<?> getAllNotices(@PageableDefault(size = 10, sort = "createdTime", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<NoticeListResDto> noticeList = noticeService.noticeList(pageable);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "공지사항 목록을 조회합니다.", noticeList);
+        return new ResponseEntity<>(commonResDto, HttpStatus.OK);
     }
 
-    @GetMapping("detail/{id}")
-    public String getNoticeDetail(@PathVariable Long id, Model model) {
-        NoticeDetailDto noticeDetail = noticeService.getNoticeDetail(id);
-        model.addAttribute("notice", noticeDetail);
-        return "board/notice/detail";
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<?> getNoticeDetail(@PathVariable Long id) {
+        try {
+            NoticeDetailDto noticeDetail = noticeService.getNoticeDetail(id);
+            CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "공지사항 상세 정보를 조회합니다.", noticeDetail);
+            return new ResponseEntity<>(commonResDto, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            e.printStackTrace();
+            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.NOT_FOUND, e.getMessage());
+            return new ResponseEntity<>(commonErrorDto, HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PostMapping("update/{id}")
-    public String noticeUpdate(@PathVariable Long id, @ModelAttribute NoticeUpdateDto dto){
-        noticeService.noticeUpdate(id, dto);
-        return "redirect:/notice/detail/" + id;
+    @PostMapping("/update/{id}")
+    public ResponseEntity<?> noticeUpdate(@PathVariable Long id, @ModelAttribute NoticeUpdateDto dto) {
+        try {
+            noticeService.noticeUpdate(id, dto);
+            CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "공지사항이 성공적으로 업데이트되었습니다.", id);
+            return new ResponseEntity<>(commonResDto, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            e.printStackTrace();
+            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.NOT_FOUND, e.getMessage());
+            return new ResponseEntity<>(commonErrorDto, HttpStatus.NOT_FOUND);
+        }
     }
 
-    @GetMapping("delete/{id}")
-    public String noticeDelete(@PathVariable Long id, Model model){
-        noticeService.noticeDelete(id);
-        return "redirect:/notice/list";
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<?> noticeDelete(@PathVariable Long id) {
+        try {
+            noticeService.noticeDelete(id);
+            CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "공지사항이 성공적으로 삭제되었습니다.", id);
+            return new ResponseEntity<>(commonResDto, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            e.printStackTrace();
+            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.NOT_FOUND, e.getMessage());
+            return new ResponseEntity<>(commonErrorDto, HttpStatus.NOT_FOUND);
+        }
     }
 }
