af8e4261fceec0b4606ba25889d4bf7f8af12c4c
KiWing <92567807+Ki-Wing@users.noreply.github.com>
2024-08-07 21:06:27 +0900

    fix: Update WishService.java

--- code/teenkiri/src/main/java/com/beyond/teenkiri/wish/service/WishService.java
+++ code/teenkiri/src/main/java/com/beyond/teenkiri/wish/service/WishService.java
@@ -1,5 +1,6 @@
 package com.beyond.teenkiri.wish.service;
 
+import com.beyond.teenkiri.subject.dto.SubjectListResDto;
 import com.beyond.teenkiri.user.domain.User;
 import com.beyond.teenkiri.wish.domain.Wish;
 import com.beyond.teenkiri.wish.repository.WishRepository;
@@ -16,20 +17,20 @@ public class WishService {
     @Autowired
     private WishRepository wishRepository;
 
-    public List<CourseListResDto> getUserWishList(User user) {
+    public List<SubjectListResDto> getUserWishList(User user) {
         List<Wish> wishes = wishRepository.findByUser(user);
-        List<CourseListResDto> courseList = new ArrayList<>();
+        List<SubjectListResDto> subjectList = new ArrayList<>();
 
         for (Wish wish : wishes) {
-            courseList.add(wish.getCourse().fromListEntity());
+            subjectList.add(wish.getSubject().fromListEntity());
         }
 
-        return courseList;
+        return subjectList;
     }
 
 
-    public void removeWish(User user, Long courseId) {
-        Wish wish = wishRepository.findByUserAndCourseId(user, courseId)
+    public void removeWish(User user, Long subjectId) {
+        Wish wish = wishRepository.findByUserAndCourseId(user, subjectId)
                 .orElseThrow(() -> new RuntimeException("찜 목록에서 해당 강좌를 찾을 수 없습니다."));
         wishRepository.delete(wish);
     }
