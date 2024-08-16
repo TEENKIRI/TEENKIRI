3ce2976afcfe0ef80072410ab35a8ea8a972ebd6
JUNG-EUN1997 <rro0628@gmail.com>
2024-08-16 16:09:03 +0900

    feat : git log 미업로드를 위한 캐시삭제

--- code/teenkiri/src/main/java/com/beyond/teenkiri/enrollment/domain/CalType.java
+++
@@ -1,7 +0,0 @@
-package com.beyond.teenkiri.enrollment.domain;
-
-// 유저 진행율 초 > progress로 변경하기위해 서비스 제작
-// 계산할 때 type구분용 enum
-public enum CalType {
-    DURATION, ISCOMPLETED
-}
--- code/teenkiri/src/main/java/com/beyond/teenkiri/user/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package com.beyond.teenkiri.user.domain;
-
-public enum Role {
-    ADMIN, TEACHER, STUDENT
-}
