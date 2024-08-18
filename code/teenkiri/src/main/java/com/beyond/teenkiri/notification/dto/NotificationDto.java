2f8492f2b3124ebe09d0750e1f05d8c0a93e1352
milcho0604 <milcho0604@gmail.com>
2024-08-18 19:12:35 +0900

    bug: 게시판 글쓰기 종류 선택 오류 해결

--- code/teenkiri/src/main/java/com/beyond/teenkiri/notification/dto/NotificationDto.java
+++ code/teenkiri/src/main/java/com/beyond/teenkiri/notification/dto/NotificationDto.java
@@ -48,7 +48,6 @@ public class NotificationDto implements Serializable  {
                 .delYN(DelYN.N)
                 .build();
     }
-
     public NotificationListDto listFromEntity() {
         return NotificationListDto.builder()
                 .id(this.id)
