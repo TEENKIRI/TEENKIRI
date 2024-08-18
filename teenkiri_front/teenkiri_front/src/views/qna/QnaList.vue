f0644af917dd48493a17d7dfc292dfc032a6b437
milcho0604 <milcho0604@gmail.com>
2024-08-18 11:14:25 +0900

    feat: 알림내역없으면 알림이 없습니다 뜨도록 수정, 질문 검색 기능 추가

--- teenkiri_front/teenkiri_front/src/views/qna/QnaList.vue
+++ teenkiri_front/teenkiri_front/src/views/qna/QnaList.vue
@@ -2,6 +2,37 @@
   <div class="board-container">
     <div class="inner">
       <h1 class="board-title">QnA 목록</h1>
+
+      <div class="filters">
+        <v-row>
+          <v-col cols="12" md="3">
+            <v-select
+              v-model="selectedSubject"
+              :items="subjects"
+              label="강좌 분류"
+              item-text="title"
+              item-value="id"
+              @change="fetchQuestions"
+            ></v-select>
+          </v-col>
+          <v-col cols="12" md="3">
+            <v-select
+              v-model="searchCategory"
+              :items="searchCategories"
+              label="검색 범위"
+            ></v-select>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field
+              v-model="searchQuery"
+              label="검색어"
+              append-icon="mdi-magnify"
+              @keyup.enter="fetchQuestions"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </div>
+
       <table class="tbl_list">
         <caption></caption>
         <colgroup>
@@ -30,9 +61,11 @@
           </tr>
         </tbody>
       </table>
+
       <div class="btnWrap">
         <button @click="createNewQuestion" class="btn_write">질문 작성하기</button>
       </div>
+
       <div class="pagingWrap">
         <ul>
           <li><a href="javascript:void(0)" @click="goToPage(1)" class="btn_paging_start"></a></li>
@@ -59,33 +92,66 @@ export default {
       totalPages: 1,
       itemsPerPage: 10,
       userRole: '',
+      searchCategory: '전체',
+      searchQuery: '',
+      selectedSubject: '', // 선택한 강좌
+      subjects: [], // 강좌 리스트
+      searchCategories: ['전체', '제목', '작성자'],
     };
   },
   created() {
+    this.fetchSubjects();
     this.fetchQuestions();
     this.userRole = localStorage.getItem('role');
   },
   methods: {
-    async fetchQuestions() {
+    async fetchSubjects() {
       try {
-        const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/qna/list`, {
-          params: {
-            page: this.currentPage - 1,
-            size: this.itemsPerPage,
-          },
-        });
-
-        const result = response.data.result;
-        if (result && result.content) {
-          this.questions = result.content;
-          this.totalPages = result.totalPages;
-        } else {
-          console.error('올바르지 않은 데이터 형식입니다:', response.data);
-        }
+        const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/subject/list`);
+        this.subjects = [{ id: '', title: '전체' }, ...response.data.result.content.map(subject => ({
+          id: subject.id,
+          title: subject.title,
+        }))];
       } catch (error) {
-        console.error('질문 목록을 불러오는 중 오류가 발생했습니다:', error);
+        console.error('강좌 목록을 불러오는 중 오류가 발생했습니다:', error);
       }
     },
+    async fetchQuestions() {
+  try {
+    const params = {
+      page: this.currentPage - 1,
+      size: this.itemsPerPage,
+      searchCategory: this.searchCategory,
+      searchQuery: this.searchQuery,
+    };
+
+    // subjectId가 ''(전체)인 경우 파라미터에서 삭제
+    if (this.selectedSubject !== '') {
+      params.subjectId = this.selectedSubject;
+    }
+
+    // 검색 범위가 전체인 경우, 제목, 작성자, 강좌명 모두 검색하도록 처리
+    if (this.searchCategory === '전체' && this.searchQuery) {
+      // 서버에서 '제목', '작성자', '강좌명'을 모두 검색하는 쿼리를 처리할 수 있도록
+      params.searchCategory = 'all'; // 'all'은 예시이므로 서버에서 처리할 수 있도록 설정
+    }
+
+    const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/qna/list`, { params });
+
+    const result = response.data.result;
+    if (result && result.content) {
+      this.questions = result.content;
+      this.totalPages = result.totalPages;
+    } else {
+      console.error('올바르지 않은 데이터 형식입니다:', response.data);
+    }
+  } catch (error) {
+    console.error('질문 목록을 불러오는 중 오류가 발생했습니다:', error);
+  }
+},
+
+
+
     formatDate(date) {
       const d = new Date(date);
       const year = d.getFullYear();
@@ -137,6 +203,10 @@ export default {
 .board-title {
   font-size: 26px;
   font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.filters {
   margin-bottom: 20px;
 }
 
