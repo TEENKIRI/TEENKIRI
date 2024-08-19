c6a11207a3a4d92d8c0a13c3b1b49ace6800a7e5
ki-wing <dldpsk151@gmail.com>
2024-08-19 09:54:42 +0900

    feat: Add PreAuthorize

--- code/teenkiri/src/main/java/com/beyond/teenkiri/subject/controller/SubjectController.java
+++ code/teenkiri/src/main/java/com/beyond/teenkiri/subject/controller/SubjectController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -75,6 +76,7 @@ public class SubjectController {
         return new ResponseEntity<>(commonResDto,HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> subjectCreate(@ModelAttribute SubjectSaveReqDto dto,
                                            @RequestPart(value = "subjectThum", required = false) MultipartFile subjectThum){
