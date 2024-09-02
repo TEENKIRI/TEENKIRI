bd02a6bc2c6f699fbdc19b39bd7248fdd7c4df5a
milcho0604 <milcho0604@gmail.com>
2024-09-02 12:28:59 +0900

    fix

--- code/teenkiri/src/main/java/com/beyond/teenkiri/user/dto/UserEditReqDto.java
+++ code/teenkiri/src/main/java/com/beyond/teenkiri/user/dto/UserEditReqDto.java
@@ -10,16 +10,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserEditReqDto {
     private String nickname;
+    private String name;
     private String password;
     private String confirmPassword;
     private Address address;
 
     @Builder
-    public UserEditReqDto(String nickname, String password, String confirmPassword, Address address) {
+    public UserEditReqDto(String nickname, String password, String confirmPassword, Address address, String name) {
         this.nickname = nickname;
         this.password = password;
         this.confirmPassword = confirmPassword;
         this.address = address;
+        this.name = name;
     }
 
 
@@ -28,6 +30,7 @@ public class UserEditReqDto {
                 .nickname(this.nickname)
                 .password(this.password)
                 .address(this.address)
+                .name(this.name)
                 .build();
     }
 }
